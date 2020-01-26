ae864e094de905337c3c2dcc89a7203fe29af43a
Yafira <yafira.martinez@lc.cuny.edu>
2020-01-25 20:04:57 -0500

    Small edits

--- Bootcamp/06 React - Base Features - Lists, Styling Components/05 components-and-elements/src/People/People.js
+++ Bootcamp/06 React - Base Features - Lists, Styling Components/05 components-and-elements/src/People/People.js
@@ -11,32 +11,25 @@ export default class People extends Component {
                     name: 'John',
                     age: 30,
                     employed: false,
-                    hobbies: ['computers', 'games', 'movies', 'reading']
+                    hobbies: ['computers', 'games', 'yoga', 'reading']
                 },
                 {
                     id: 1,
                     name: 'Jane',
                     age: 20,
                     employed: false,
-                    hobbies: ['drinking', 'dancing', 'cute stuff']
-                },
-                {
-                    id: 2,
-                    name: 'June',
-                    age: 30,
-                    employed: true,
-                    hobbies: ['pachinko', 'martial arts']
+                    hobbies: ['martial arts', 'dancing', 'kawaii stuff'],
                 }
             ],
             showPersons: false
     }
-    
+
 
     handlePerson = (newName, index = 0) => {
         this.setState(prevState => {
             let newState = {...prevState};
             newState.people[index].name = newName;
-            
+
             return newState;
         })
     }
@@ -44,8 +37,8 @@ export default class People extends Component {
     // deep clone: clone both the array AND the object that we want to modify
 
     // if we just clone the array into a new reference then modify one of its objects, we are actually modifying the original object on the original array, because they have the same reference!
-    nameChangedHandler = (i, e) => {  
-        const value = e.target.value;     
+    nameChangedHandler = (i, e) => {
+        const value = e.target.value;
         this.setState(prevState => {
             const newPeople = [...prevState.people];
             const selected = newPeople.findIndex(person => Number(person.id) === Number(i));
@@ -54,7 +47,7 @@ export default class People extends Component {
             newPeople[selected] = wanted;
             return { people: newPeople }
         })
-    }   
+    }
 
     togglePersonsHandler = () => {
         this.setState(prevState => ({
@@ -62,7 +55,7 @@ export default class People extends Component {
         }))
     }
 
-    deletePersonHandler = k => {        
+    deletePersonHandler = k => {
         this.setState(prevState => {
             const newPeople = Object.assign([], prevState.people);
             const toBeDeleted = newPeople.findIndex(person => person.id === Number(k));
@@ -88,24 +81,24 @@ export default class People extends Component {
             persons = (
                 <div>
 
-                    {this.state.people.map((person, index) => 
-                    <Person 
-                    name = { person.name } 
-                    age = { person.age } 
-                    employed = { person.employed } 
-                    hobbies = { person.hobbies } 
-                    id = { person.id } 
+                    {this.state.people.map((person, index) =>
+                    <Person
+                    name = { person.name }
+                    age = { person.age }
+                    employed = { person.employed }
+                    hobbies = { person.hobbies }
+                    id = { person.id }
                     key= { person.id }
                     nameChangedHandler = { this.nameChangedHandler.bind(this, person.id) }
                     click={ this.deletePersonHandler.bind(this, person.id) } />)}
 
-                    </div>                   
+                    </div>
             );
         }
 
         return (
             <div>
-                { 
+                {
                     /*
                     DO NOT USE () => ... (this way you call 2 functions instead of just one, performance hit)
 
@@ -114,8 +107,8 @@ export default class People extends Component {
                 }
 
                 <button onClick={ this.togglePersonsHandler } style={ btnStyle }>Toggle persons!</button>
-               
-                {persons}                
+
+                {persons}
             </div>
         )
     }
